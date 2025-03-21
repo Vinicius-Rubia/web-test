dd0fd2306bf741cdbb1d695a054ed0f8d7092fa5
Vinicius Rubia <vinicius.rubia@negocieonline.com.br>
2025-03-21 02:28:29 -0300

    fix: remove jsx not used

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -1,7 +1,6 @@
 import { LayoutMain } from "@/layouts/layout-main";
 import { Signin } from "@/pages/auth/signin";
 import { Panel } from "@/pages/panel";
-import { NewProject } from "@/pages/projects/new-project";
 import { NewTestCase } from "@/pages/tests/new-test-case";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
@@ -14,7 +13,7 @@ export const Router = () => {
         <Route element={<LayoutMain />}>
           <Route path="/panel" element={<Panel />} />
 
-          <Route path="/projects/new" element={<NewProject />} />
+          {/* <Route path="/projects/new" element={<NewProject />} /> */}
 
           <Route path="/testcases/new" element={<NewTestCase />} />
         </Route>
